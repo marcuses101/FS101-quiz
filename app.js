041db0c9456fea7278fdfc260afbd5c0892a81b8
marcuses101 <mnjconnolly@gmail.com>
2020-09-06 14:06:47 -0400

    reformated into class structure

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  */
 const root = document.querySelector("main");
 const store = {
+  name: "Test Quiz",
   // 5 or more questions are required
   questions: [
     {
@@ -26,15 +27,56 @@ const store = {
       correctAnswer: '2019'
     }
   ],
-  currentScreen: "start",
-  currentQuestion: {},
-  userAnswer: "",
-  isCorrect: false,
-  currentQuestionIndex: 0,
-  correctAnswers: 0
 };
 
-store.totalQuestions = store.questions.length;
+class QuizModel {
+  constructor(quizStore) {
+    this.name = quizStore.name;
+    this.questions = quizStore.questions.map((question, index) => {
+      question.userAnswer = ""
+      question.index = index;
+      question.correct = null;
+      return question;
+    });
+    this.quizLength = this.questions.length;
+    this.currentQuestion = this.questions[0];
+    this.quizComplete = false;
+  }
+  nextQuestion() {
+    if (this.currentQuestion.index + 1 < this.quizLength) {
+      this.currentQuestion = this.questions[this.currentQuestion.index + 1];
+      return this.currentQuestion
+    } else {
+      this.quizComplete = true;
+      this.returnResults();
+    }
+  }
+  answerQuestion(answerString) {
+    this.currentQuestion.userAnswer = answerString;
+    this.currentQuestion.correct = (this.currentQuestion.correctAnswer == answerString) ? true : false;
+    return this.currentQuestion.correct;
+  }
+  returnResults() {
+    console.log("results:")
+    const results = this.questions.reduce((resultObj, currentQuestion) => {
+      resultObj.questionsCorrect += currentQuestion.correct ? 1 : 0
+      resultObj.questionsAnswered += (currentQuestion.correct !== null) ? 1 : 0;
+      return resultObj;
+    }, {
+      totalQuestions: this.quizLength,
+      questionsAnswered: 0,
+      questionsCorrect: 0
+    })
+
+    return results;
+  }
+  resetQuiz() {
+    this.questions.forEach(question => question.correct = null)
+    this.quizComplete = false;
+  }
+}
+
+
 
 /**
  * 
@@ -52,59 +94,18 @@ store.totalQuestions = store.questions.length;
  */
 
 /********** TEMPLATE GENERATION FUNCTIONS **********/
-function startScreen(storeObj) {
-  return `<section id="start-screen">
-  <h2>Welcome to the Figure Skating 101 Quiz Show!</h2>
-  <button class="start">Begin!</button>
-</section>`
-}
 
-function questionScreen(storeObj) {
-  const questionNumber = storeObj.currentQuestionIndex + 1;
-  const totalQuestions = storeObj.totalQuestions;
-  const correctAnswers = storeObj.correctAnswers;
 
-  return `
-<section id="question">
-  <span>Question: ${questionNumber} of ${totalQuestions}</span>
-  <span>Current Score: ${correctAnswers}/${questionNumber}</span>
-  <h2>What is the color of a Watermelon?</h2>
-  <form action="">
-      <label for="1">Red</label>
-      <input type="radio" name="multi-answer" id="1" value="red">
-      <label for="2">Blue</label>
-      <input type="radio" name="multi-answer" id="2" value="blue">
-      <label for="3">Purple</label>
-      <input type="radio" name="multi-answer" id="3" value="purple">
-      <label for="4">Green</label>
-      <input type="radio" name="multi-answer" id="4" value="green">
-      <input type="submit">
-  </form>
-</section>`
-  // return html string of qestion interface
-}
 
-function incorrectAnswerScreen(storeObj) {
-  // return html string of answer screen
-  return `    <section id="answer">
-  <h2>Answer</h2>
-  <h3>You answered:</h3>
-  <h4>Blue</h4>
-  <h3>The correct answer was:</h3>
-  <h4>green</h4>
-  <h3>Your current score is 2/4. Don't give up!</h3>
-  <button class="next">Next</button>
-</section>`
-}
 
 function correctAnswerScreen(storeObj) {
   return `    <section id="answer">
   <h2>Answer</h2>
   <h3>You answered:</h3>
-  <h4>Blue</h4>
+  <h4>${storeObj.userAnswer}</h4>
   <h3>The correct answer was:</h3>
-  <h4>green</h4>
-  <h3>Your current score is 2/4. Don't give up!</h3>
+  <h4>${storeObj.currentQuestion.correctAnswer}</h4>
+  <h3>Your current score is ${storeObj.correctAnswers}/${storeObj.currentQuestionIndex}. Good Job!</h3>
   <button class="next">Next</button>
 </section>`
 }
@@ -113,101 +114,100 @@ function resultScreen(storeObj) {
   return `<section id="results">
           <h2>Results:</h2>
           <h3>Your final score is:</h3>
-          <h4>2/5 better luck next time!</h4>
+          <h4>${storeObj.correctAnswers}/${storeObj.totalQuestions} better luck next time!</h4>
         </section> `
   // return html string of answer screen
 }
 // These functions return HTML templates
 
 /********** RENDER FUNCTION(S) **********/
-function render(element, storeObj) {
-  console.log("rendering")
-  const screens = {
-    start: startScreen,
-    question: questionScreen,
-    correctAnswer: correctAnswerScreen,
-    incorrectAnswer: incorrectAnswerScreen,
-    result: resultScreen
+class QuizView {
+  constructor(rootElement) {
+    this.target = rootElement;
+    this.currentView = "<h2>Hello World</h2>";
   }
-  console.log(storeObj.currentScreen)
-  const html = screens[storeObj.currentScreen](storeObj);
-  $(element).html(html)
-  // choose which screen to run
+  start() {
+    this.currentView = `<section id="start-screen">
+    <h2>Welcome to the Figure Skating 101 Quiz Show!</h2>
+    <button class="start">Begin!</button>
+    </section>`
+  }
+  question(question, results) {
+    this.currentView = `<section id="question">
+                          <p>Question ${results.questionsAnswered + 1} of ${results.totalQuestions}</p>
+                          <p>Current Score: ${results.questionsCorrect}/${results.questionsAnswered}</p>
+                          <h2>${question.question}</h2>
+                          <form action="">
+                          <input type="radio" name="multi-answer" id="1" value="${question.answers[0]}">
+                          <label for="1">${question.answers[0]}</label>
+                          <input type="radio" name="multi-answer" id="2" value="${question.answers[1]}">
+                          <label for="2">${question.answers[1]}</label>
+                          <input type="radio" name="multi-answer" id="3" value="${question.answers[2]}">
+                          <label for="3">${question.answers[2]}</label>
+                          <input type="radio" name="multi-answer" id="4" value="${question.answers[3]}">
+                          <label for="4">${question.answers[3]}</label>
+                              <input type="submit">
+                          </form>
+                        </section>`
+  }
+  answer(question, result) {
+    this.currentView = ``;
+  }
+  results(results) {
+    this.currentView = ``;
+  }
+  render() {
+    $(this.target).html(this.currentView)
+  }
 }
 
-function chooseNextScreen(storeObj) {
-  console.log("choosing")
-  console.log(storeObj.currentScreen)
-  if (storeObj.currentScreen === "start") {
-    storeObj.currentScreen = "question";
-    console.log("screen changed")
-  } else if (storeObj.currentScreen == 'question') {
-    storeObj.currentScreen = storeObj.isCorrect ? "correctAnswer" : "incorrectAnswer";
-  } else if (storeObj.currentScreen.includes("Answer")) {
-    storeObj.currentScreen = 'question';
-  }
-}
+// choose which screen to run
 
-function answerQuestion(storeObj, answer) {
-  if (storeObj.currentQuestion.correctAnswer === answer) {
-    storeObj.isCorrect = true;
-    console.log('correct!')
-  } else {
-    storeObj.isCorrect = false;
-    console.log("not correct :(")
-  }
-}
-function loadQuestion(storeObj) {
-  storeObj.currentQuestion = storeObj.questions[storeObj.currentQuestionIndex]
-}
 
 /********** EVENT HANDLER FUNCTIONS **********/
 
-function addSubmitHandler(element, storeObj) {
-  $(element).on("submit", "form", function (event) {
+class QuizController {
+  constructor(rootElement, quizModel, quizView) {
+    this.root = rootElement;
+    this.model = quizModel;
+    this.view = quizView;
+  }
+  handleStart() {
+    this.view.question(this.model.currentQuestion,this.model.returnResults());
+    this.view.render();
+    console.log("start")
+  }
+
+  handleSubmit(event) {
     event.preventDefault();
-    const answer = $(this).children("input[name='multi-answer']:checked").val();
-    answerQuestion(storeObj, answer)
-    chooseNextScreen(storeObj)
-    render(element, storeObj)
-    loadQuestion(storeObj)
-  })
-  console.log('submit handling')
+    const answer = $(event.target).find("input:checked").val();
+    this.model.answerQuestion(answerString);
+    
+    console.log(answer);
+  }
+
+  handleNext() {
+    console.log("next")
+  }
+  setEventListeners() {
+    $(this.root).on("click", ".start", $.proxy(this.handleStart,this));
+    $(this.root).on("click", ".next", $.proxy(this.handleNext,this));
+    $(this.root).on("submit", "form", $.proxy(this.handleSubmit,this));
+  }
+  startApp() {
+    this.setEventListeners();
+    this.view.start();
+    this.view.render();
+  }
 }
 
-
-
-function addStartHandler(element, storeObj) {
-  $(element).on("click", ".start", function (event) {
-    loadQuestion(storeObj);
-    chooseNextScreen(storeObj);
-    render(element, storeObj)
-    console.log(event)
-    console.log("start clicked")
-  })
-  console.log('start handling')
-}
-
-function addNextHandler(element, storeObj) {
-  $(element).on("click", ".next", function (event) {
-    storeObj.currentQuestionIndex++;
-    chooseNextScreen(storeObj);
-    render(element, storeObj)
-  })
-}
-
-
-function startApp(element, storeObj) {
-  addStartHandler(element, storeObj)
-  addSubmitHandler(element, storeObj)
-  addNextHandler(element, storeObj)
-  render(element, storeObj);
-}
+const testQuizModel = new QuizModel(store);
+const testQuizView = new QuizView(root);
+const testQuizController = new QuizController(root, testQuizModel, testQuizView);
 // These functions handle events (submit, click, etc)
 
 
 //initialize on page load
-
 $(() => {
-  startApp(root, store)
+  testQuizController.startApp();
 })
