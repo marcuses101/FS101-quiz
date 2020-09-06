fbf83c979ea20d83c73ab83502ad0353b68fc19c
marcuses101 <mnjconnolly@gmail.com>
2020-09-06 00:30:18 -0400

    logic added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /**
  * Example store structure
  */
+const root = document.querySelector("main");
 const store = {
   // 5 or more questions are required
   questions: [
@@ -25,9 +26,12 @@ const store = {
       correctAnswer: '2019'
     }
   ],
-  quizStarted: false,
-  questionNumber: 0,
-  score: 0
+  currentScreen: "start",
+  currentQuestion: {},
+  userAnswer: "",
+  isCorrect: false,
+  currentQuestionIndex: 0,
+  correctAnswers: 0
 };
 
 store.totalQuestions = store.questions.length;
@@ -48,44 +52,162 @@ store.totalQuestions = store.questions.length;
  */
 
 /********** TEMPLATE GENERATION FUNCTIONS **********/
-function introScreen (store) {
-  // return html string of intro
+function startScreen(storeObj) {
+  return `<section id="start-screen">
+  <h2>Welcome to the Figure Skating 101 Quiz Show!</h2>
+  <button class="start">Begin!</button>
+</section>`
 }
 
-function questionScreen (store) {
+function questionScreen(storeObj) {
+  const questionNumber = storeObj.currentQuestionIndex + 1;
+  const totalQuestions = storeObj.totalQuestions;
+  const correctAnswers = storeObj.correctAnswers;
+
+  return `
+<section id="question">
+  <span>Question: ${questionNumber} of ${totalQuestions}</span>
+  <span>Current Score: ${correctAnswers}/${questionNumber}</span>
+  <h2>What is the color of a Watermelon?</h2>
+  <form action="">
+      <label for="1">Red</label>
+      <input type="radio" name="multi-answer" id="1" value="red">
+      <label for="2">Blue</label>
+      <input type="radio" name="multi-answer" id="2" value="blue">
+      <label for="3">Purple</label>
+      <input type="radio" name="multi-answer" id="3" value="purple">
+      <label for="4">Green</label>
+      <input type="radio" name="multi-answer" id="4" value="green">
+      <input type="submit">
+  </form>
+</section>`
   // return html string of qestion interface
 }
 
-function answerScreen () {
+function incorrectAnswerScreen(storeObj) {
   // return html string of answer screen
+  return `    <section id="answer">
+  <h2>Answer</h2>
+  <h3>You answered:</h3>
+  <h4>Blue</h4>
+  <h3>The correct answer was:</h3>
+  <h4>green</h4>
+  <h3>Your current score is 2/4. Don't give up!</h3>
+  <button class="next">Next</button>
+</section>`
 }
 
-function resultScreen () {
+function correctAnswerScreen(storeObj) {
+  return `    <section id="answer">
+  <h2>Answer</h2>
+  <h3>You answered:</h3>
+  <h4>Blue</h4>
+  <h3>The correct answer was:</h3>
+  <h4>green</h4>
+  <h3>Your current score is 2/4. Don't give up!</h3>
+  <button class="next">Next</button>
+</section>`
+}
+
+function resultScreen(storeObj) {
+  return `<section id="results">
+          <h2>Results:</h2>
+          <h3>Your final score is:</h3>
+          <h4>2/5 better luck next time!</h4>
+        </section> `
   // return html string of answer screen
 }
 // These functions return HTML templates
 
 /********** RENDER FUNCTION(S) **********/
+function render(element, storeObj) {
+  console.log("rendering")
+  const screens = {
+    start: startScreen,
+    question: questionScreen,
+    correctAnswer: correctAnswerScreen,
+    incorrectAnswer: incorrectAnswerScreen,
+    result: resultScreen
+  }
+  console.log(storeObj.currentScreen)
+  const html = screens[storeObj.currentScreen](storeObj);
+  $(element).html(html)
+  // choose which screen to run
+}
 
-function render() {
+function chooseNextScreen(storeObj) {
+  console.log("choosing")
+  console.log(storeObj.currentScreen)
+  if (storeObj.currentScreen === "start") {
+    storeObj.currentScreen = "question";
+    console.log("screen changed")
+  } else if (storeObj.currentScreen == 'question') {
+    storeObj.currentScreen = storeObj.isCorrect ? "correctAnswer" : "incorrectAnswer";
+  } else if (storeObj.currentScreen.includes("Answer")) {
+    storeObj.currentScreen = 'question';
+  }
+}
 
-  // This function conditionally replaces the contents of the <main> tag based on the state of the store
-
+function answerQuestion(storeObj, answer) {
+  if (storeObj.currentQuestion.correctAnswer === answer) {
+    storeObj.isCorrect = true;
+    console.log('correct!')
+  } else {
+    storeObj.isCorrect = false;
+    console.log("not correct :(")
+  }
+}
+function loadQuestion(storeObj) {
+  storeObj.currentQuestion = storeObj.questions[storeObj.currentQuestionIndex]
 }
 
 /********** EVENT HANDLER FUNCTIONS **********/
 
-function handleSubmit () {
-  
+function addSubmitHandler(element, storeObj) {
+  $(element).on("submit", "form", function (event) {
+    event.preventDefault();
+    const answer = $(this).children("input[name='multi-answer']:checked").val();
+    answerQuestion(storeObj, answer)
+    chooseNextScreen(storeObj)
+    render(element, storeObj)
+    loadQuestion(storeObj)
+  })
+  console.log('submit handling')
 }
 
-function handleStart () {
 
+
+function addStartHandler(element, storeObj) {
+  $(element).on("click", ".start", function (event) {
+    loadQuestion(storeObj);
+    chooseNextScreen(storeObj);
+    render(element, storeObj)
+    console.log(event)
+    console.log("start clicked")
+  })
+  console.log('start handling')
 }
 
-function initializeListeners () {
-  //start event listeners
+function addNextHandler(element, storeObj) {
+  $(element).on("click", ".next", function (event) {
+    storeObj.currentQuestionIndex++;
+    chooseNextScreen(storeObj);
+    render(element, storeObj)
+  })
+}
 
 
+function startApp(element, storeObj) {
+  addStartHandler(element, storeObj)
+  addSubmitHandler(element, storeObj)
+  addNextHandler(element, storeObj)
+  render(element, storeObj);
 }
 // These functions handle events (submit, click, etc)
+
+
+//initialize on page load
+
+$(() => {
+  startApp(root, store)
+})
