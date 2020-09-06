af4b86bce5065aaedd98c7d900b910091bdce5cf
marcuses101 <mnjconnolly@gmail.com>
2020-09-06 18:03:03 -0400

    add skating questions

--- app.js
+++ app.js
@@ -1,31 +1,66 @@
+
 /**
  * Example store structure
  */
 const root = document.querySelector("main");
 const store = {
-  name: "Test Quiz",
+  name: "Figure Skating 101",
   // 5 or more questions are required
   questions: [
     {
-      question: 'What color is broccoli?',
+      question: 'What is the take-ff edge of an Axel jump element?',
       answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+        'Forward Outside',
+        'Forward Inside',
+        'Backward Outside',
+        'Backward Inside'
       ],
-      correctAnswer: 'green'
+      correctAnswer: 'Forward Outside'
     },
     {
-      question: 'What is the current year?',
+      question: `What is the exit edge of a Left Forward Outside Rocker turn`,
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
+        'Left Backward Outside',
+        'Left Backward Inside',
+        'Right Backward Outside',
+        'Right Backward Outside'
       ],
-      correctAnswer: '2019'
+      correctAnswer: 'Left Backward Outside'
+    },
+    {
+      question: `What is the basic position of this spin element?`,
+      imgURL: "https://upload.wikimedia.org/wikipedia/commons/6/6e/Sit_spin.jpg",
+      answers: [
+        'Sit',
+        'Camel',
+        'Upright',
+        'Non-basic'
+      ],
+      correctAnswer: "Sit"
+    },
+    {
+      question: "What is the name of this field movement?",
+      imgURL: "https://i.pinimg.com/originals/70/b3/d7/70b3d72afe1681035fbcc270b36bd7c5.jpg",
+      answers: [
+        'Spiral',
+        'Death Spiral',
+        'Ina Bauer',
+        'Spread Eagle'
+      ],
+      correctAnswer: "Spread Eagle"
+    },
+    {
+      question: "What jump is this?",
+      imgURL: "https://cdn.theatlantic.com/thumbor/El2KuNVfAZzI8xf2Ndc9xPNWPRk=/media/img/posts/2014/02/kimyunatum/original.gif",
+      answers: [
+        "Triple Axel",
+        "Double Toe",
+        "Double Axel",
+        "Quadruple Lutz"
+      ],
+      correctAnswer: "Double Axel"
     }
+
   ],
 };
 
@@ -35,7 +70,7 @@ class QuizModel {
     this.questions = quizStore.questions.map((question, index) => {
       question.userAnswer = ""
       question.index = index;
-      question.correct = null;
+      question.answerIsCorrect = null;
       return question;
     });
     this.quizLength = this.questions.length;
@@ -43,9 +78,9 @@ class QuizModel {
     this.quizComplete = false;
   }
   nextQuestion() {
-    if (this.currentQuestion.index + 1 < this.quizLength) {
+    if (this.currentQuestion.index < this.quizLength -1) {
       this.currentQuestion = this.questions[this.currentQuestion.index + 1];
-      return this.currentQuestion
+      console.log(this.currentQuestion)
     } else {
       this.quizComplete = true;
       this.returnResults();
@@ -53,14 +88,14 @@ class QuizModel {
   }
   answerQuestion(answerString) {
     this.currentQuestion.userAnswer = answerString;
-    this.currentQuestion.correct = (this.currentQuestion.correctAnswer == answerString) ? true : false;
-    return this.currentQuestion.correct;
+    this.currentQuestion.answerIsCorrect = (this.currentQuestion.correctAnswer == answerString) ? true : false;
+    return this.currentQuestion.answerIsCorrect;
   }
   returnResults() {
     console.log("results:")
     const results = this.questions.reduce((resultObj, currentQuestion) => {
-      resultObj.questionsCorrect += currentQuestion.correct ? 1 : 0
-      resultObj.questionsAnswered += (currentQuestion.correct !== null) ? 1 : 0;
+      resultObj.questionsCorrect += currentQuestion.answerIsCorrect ? 1 : 0
+      resultObj.questionsAnswered += (currentQuestion.answerIsCorrect !== null) ? 1 : 0;
       return resultObj;
     }, {
       totalQuestions: this.quizLength,
@@ -71,51 +106,15 @@ class QuizModel {
     return results;
   }
   resetQuiz() {
-    this.questions.forEach(question => question.correct = null)
+    this.questions.forEach(question => question.answerIsCorrect = null)
     this.quizComplete = false;
+    this.currentQuestion = this.questions[0];
   }
 }
 
 
-
-/**
- * 
- * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
- * See your course material and access support for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
- * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
- */
-
-/********** TEMPLATE GENERATION FUNCTIONS **********/
-
-
-
-
-function correctAnswerScreen(storeObj) {
-  return `    <section id="answer">
-  <h2>Answer</h2>
-  <h3>You answered:</h3>
-  <h4>${storeObj.userAnswer}</h4>
-  <h3>The correct answer was:</h3>
-  <h4>${storeObj.currentQuestion.correctAnswer}</h4>
-  <h3>Your current score is ${storeObj.correctAnswers}/${storeObj.currentQuestionIndex}. Good Job!</h3>
-  <button class="next">Next</button>
-</section>`
-}
-
 function resultScreen(storeObj) {
-  return `<section id="results">
-          <h2>Results:</h2>
-          <h3>Your final score is:</h3>
-          <h4>${storeObj.correctAnswers}/${storeObj.totalQuestions} better luck next time!</h4>
-        </section> `
+  return 
   // return html string of answer screen
 }
 // These functions return HTML templates
@@ -126,14 +125,14 @@ class QuizView {
     this.target = rootElement;
     this.currentView = "<h2>Hello World</h2>";
   }
-  start() {
+  start(quizName) {
     this.currentView = `<section id="start-screen">
-    <h2>Welcome to the Figure Skating 101 Quiz Show!</h2>
+    <h2>Welcome to the ${quizName} Quiz Show!</h2>
     <button class="start">Begin!</button>
     </section>`
   }
   question(question, results) {
-    this.currentView = `<section id="question">
+    let htmlString = `<section id="question">
                           <p>Question ${results.questionsAnswered + 1} of ${results.totalQuestions}</p>
                           <p>Current Score: ${results.questionsCorrect}/${results.questionsAnswered}</p>
                           <h2>${question.question}</h2>
@@ -149,12 +148,32 @@ class QuizView {
                               <input type="submit">
                           </form>
                         </section>`
+
+  if (question.imgURL) {
+    let tree = $(`<div>${htmlString}</div>`)
+    tree.find("h2").after(`<img src="${question.imgURL}">`)
+    htmlString = tree.html();
   }
-  answer(question, result) {
-    this.currentView = ``;
+  this.currentView = htmlString;
+  }
+  answer(question, results) {
+    this.currentView = `<section id="answer">
+                          <h2>Answer</h2>
+                          <h3>You answered:</h3>
+                          <h4>${question.userAnswer}</h4>
+                          <h3>The correct answer was:</h3>
+                          <h4>${question.correctAnswer}</h4>
+                          <h3>Your current score is ${results.questionsCorrect}/${results.questionsAnswered}. Good Job!</h3>
+                          <button class="next">Next</button>
+                        </section>`;
   }
   results(results) {
-    this.currentView = ``;
+    this.currentView = `<section id="results">
+                          <h2>Results:</h2>
+                          <h3>Your final score is:</h3>
+                          <h4>${results.questionsCorrect}/${results.questionsAnswered} better luck next time!</h4>
+                          <button class="start">Play again?</button>
+                        </section> `;
   }
   render() {
     $(this.target).html(this.currentView)
@@ -173,7 +192,8 @@ class QuizController {
     this.view = quizView;
   }
   handleStart() {
-    this.view.question(this.model.currentQuestion,this.model.returnResults());
+    this.model.resetQuiz()
+    this.view.question(this.model.currentQuestion, this.model.returnResults());
     this.view.render();
     console.log("start")
   }
@@ -181,22 +201,27 @@ class QuizController {
   handleSubmit(event) {
     event.preventDefault();
     const answer = $(event.target).find("input:checked").val();
-    this.model.answerQuestion(answerString);
-    
-    console.log(answer);
+    this.model.answerQuestion(answer);
+    this.view.answer(this.model.currentQuestion, this.model.returnResults())
+    this.view.render();
   }
 
   handleNext() {
     console.log("next")
+    this.model.nextQuestion();
+    this.model.quizComplete 
+    ? this.view.results(this.model.returnResults())
+    : this.view.question(this.model.currentQuestion,this.model.returnResults());
+    this.view.render();
   }
   setEventListeners() {
-    $(this.root).on("click", ".start", $.proxy(this.handleStart,this));
-    $(this.root).on("click", ".next", $.proxy(this.handleNext,this));
-    $(this.root).on("submit", "form", $.proxy(this.handleSubmit,this));
+    $(this.root).on("click", ".start", $.proxy(this.handleStart, this));
+    $(this.root).on("click", ".next", $.proxy(this.handleNext, this));
+    $(this.root).on("submit", "form", $.proxy(this.handleSubmit, this));
   }
   startApp() {
     this.setEventListeners();
-    this.view.start();
+    this.view.start(this.model.name);
     this.view.render();
   }
 }
